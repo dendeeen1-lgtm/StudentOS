447473776209838dfc6f2ded3cb605bcf516e929
dendeeen1-lgtm <dendeeen1@gmail.com>
2026-06-20 11:21:47 +0800

    Fix: add index.js entry point and fix metro config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,5 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
 
 module.exports = config;
